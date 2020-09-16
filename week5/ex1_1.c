b2e82c25836f88879b6e686441e96a592415e5fb
emileyray <69918609+emileyray@users.noreply.github.com>
2020-09-16 22:54:25 +0300

    Create ex1_1.c

---
+++ week5/ex1_1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+void *print_message_function(void *ptr)
+{
+     char *message;
+     message = (char *) ptr;
+     printf("%s \n", message);
+}
+int main()
+{
+     long N = 5; //number of threads
+     pthread_t thread_id[100]; //the threads
+     int iret[100]; //status of threads, 0 if ok
+
+     for (long i = 0; i < N; i++){
+        char* message; //the message to be printed
+        sprintf(message, "Thread %ld is being created", i);
+        
+        iret[i] = pthread_create( &thread_id[i], NULL, print_message_function, (void*) message); //creating a threads
+        
+        printf("Thread %ld returns: %d\n",i, iret[i]); //printing status
+     }
+     
+     exit(0);
+}
